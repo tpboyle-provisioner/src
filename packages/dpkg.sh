4adec9b785b1a207ef6dedcb86bf301f4f03fd9d
Patrick Boyle <t.patrick.boyle@pm.me>
2025-09-13 11:10:21 -0500

    cleaned up dpkg.sh

--- packages/dpkg.sh
+++ packages/dpkg.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+
+# CONFIG
+
 DEB_LOCATION="/tmp/install.deb"
+
+
+# INTERFACE
+
+dpkg_ensure_package_is_installed () {
+  local package="$1"
+  local url="$2"
+  if ! dpkg_package_is_installed "$package"; then
+    dpkg_install_deb_from_url "$url"
+  fi
+}
+
+
+# IMPLEMENTATION
 
 dpkg_package_is_installed () {
   local package="$1"
@@ -11,12 +28,4 @@ dpkg_install_deb_from_url () {
   local url="$1"
   wget -O "$DEB_LOCATION" "$url"
   sudo dpkg -i "$DEB_LOCATION"
-}
-
-dpkg_ensure_package_is_installed () {
-  local package="$1"
-  local url="$2"
-  if ! dpkg_package_is_installed "$package"; then
-    dpkg_install_deb_from_url "$url"
-  fi
 }
