3fa665a7e7217f2f0d1f4e86af85e4abec1db620
Patrick Boyle <t.patrick.boyle@pm.me>
2025-10-01 19:53:53 -0500

    + apt canonical vs. ppa repositories

--- packages/apt/repositories.sh
+++ packages/apt/repositories.sh
@@ -4,12 +4,29 @@
 APT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 
-# REPOSITORIES
+# CANONICAL REPOSITORIES
 
-apt_ppa_repository_is_installed () {
-  local source_file="$1"
-  ls /etc/apt/sources.list.d | grep -qi "$source_file"
+apt_ensure_canonical_repository_is_enabled () {
+  local name="$1"
+  if ! apt_canonical_repository_is_enabled "$name"; then
+    apt_enable_canonical_repository "$name"
+  fi
 }
+
+apt_canonical_repository_is_enabled () {
+  local repo="$1"
+  cat /etc/apt/sources.list.d/ubuntu.sources | grep ^Component | grep -q "$repo"
+}
+
+apt_enable_canonical_repository () {
+  local name="$1"
+  info "apt" "Enabling Canonical repository '$name'..."
+  sudo add-apt-repository -y "$name"
+  sudo apt update
+}
+
+
+# PPA REPOSITORIES
 
 apt_ensure_ppa_repository_is_installed () {
   local name="$1"
@@ -19,7 +36,13 @@ apt_ensure_ppa_repository_is_installed () {
   fi
 }
 
+apt_ppa_repository_is_installed () {
+  local source_file="$1"
+  ls /etc/apt/sources.list.d | grep -qiE "$source_file"
+}
+
 apt_install_ppa_repository () {
+  local name="$1"
   info "apt" "Installing PPA repository '$name'..."
   sudo add-apt-repository -y "$name"
   sudo apt update
